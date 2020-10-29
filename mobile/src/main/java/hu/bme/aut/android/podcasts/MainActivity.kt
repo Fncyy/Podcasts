982081b387badf4069d642520cff9067ec12e698
Pénzes Martin <penzes.martin7@gmail.com>
2020-10-29 18:32:03 +0100

    Fix a bug regarding navigation animations

--- mobile/src/main/java/hu/bme/aut/android/podcasts/MainActivity.kt
+++ mobile/src/main/java/hu/bme/aut/android/podcasts/MainActivity.kt
@@ -2,11 +2,17 @@ package hu.bme.aut.android.podcasts
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
+import hu.bme.aut.android.podcasts.ui.favourites.FavouritesFragment
+import hu.bme.aut.android.podcasts.ui.home.HomeFragment
+import hu.bme.aut.android.podcasts.ui.menu.MenuFragment
+import hu.bme.aut.android.podcasts.ui.search.SearchFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -22,5 +28,45 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         navView.setupWithNavController(navController)
+
+        navView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    val fragment = HomeFragment()
+                    add(fragment)
+                    setStack(fragment)
+                }
+                R.id.navigation_search -> {
+                    val fragment = SearchFragment()
+                    add(fragment)
+                    setStack(HomeFragment(), fragment)
+                }
+                R.id.navigation_favourites -> {
+                    val fragment = FavouritesFragment()
+                    add(fragment)
+                    setStack(HomeFragment(), fragment)
+                }
+                R.id.navigation_menu -> {
+                    val fragment = MenuFragment()
+                    add(fragment)
+                    setStack(HomeFragment(), fragment)
+                }
+            }
+            true
+        }
+        navView.setOnNavigationItemReselectedListener {}
+    }
+
+    private fun add(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.navHostFragment, fragment, fragment.tag)
+            .addToBackStack(fragment.tag)
+            .commit()
+    }
+
+    private fun setStack(vararg fragments: Fragment) {
+        supportFragmentManager.popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        fragments.forEach(this::add)
     }
 }
